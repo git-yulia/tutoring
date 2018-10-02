249d0df1c7eb68679badd78687ad767dcce113b5
Yulia <julia.maliauka953@myci.csuci.edu>
2018-10-02 09:56:15 -0700

    added nick simulator

--- COMP_105/prettier_example.py
+++ COMP_105/prettier_example.py
@@ -11,9 +11,14 @@ window.bgcolor('pink')
 painter = turtle.Turtle()
 painter.pencolor("blue")
 
-for i in range(50):
-    painter.forward(50)
-    painter.left(123)
-
+for i in range(500):
+    if random.randint(1,5) == 1:
+        painter.forward(20)
+    if random.randint(1,5) == 2:
+        painter.backward(20)
+    if random.randint(1,5) == 3:
+        painter.left(20)
+    if random.randint(1,5) == 4:
+        painter.right(20)
 
 window.exitonclick() 
