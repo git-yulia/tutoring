bea29a1bc95b771c03d60b5bd733af6ad755328f
Yulia <julia.maliauka953@myci.csuci.edu>
2018-10-01 22:47:51 -0700

    added another example to show my students, using painter

---
+++ COMP_105/prettier_example.py
@@ -0,0 +1,19 @@
+# COMP 105
+# Fall 2018
+
+import turtle
+import math 
+import random
+
+window = turtle.Screen()
+window.bgcolor('pink')
+
+painter = turtle.Turtle()
+painter.pencolor("blue")
+
+for i in range(50):
+    painter.forward(50)
+    painter.left(123)
+
+
+window.exitonclick() 
